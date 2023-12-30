66c661b588593684cdb46db3742e9532d12dd97f
Hamza <54952184+i-am-hamza@users.noreply.github.com>
2023-12-30 08:00:50 +0530

    Minor updates

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -6,7 +6,7 @@ function Intro (){
         
             <h1 className='text-4xl md:text-7xl mb-1 md:mb-3 font-bold'> Hamza</h1>
             <p className='text-base md:text-x1 mb-3 font-medium'>Computer Science Engineer</p>
-            <p className='text-sm max-w-xl mb-6 font-bold'>This is my entire bio</p>
+            <p className='text-sm max-w-xl mb-6 font-bold'>I am Computer Science Graduate, currently working as a Consultant in Ernst and Young (EY) India. Having 2 years of experience in Cyber Security domain and hands on projects in full stack development, I am a technophile who loves to expriment out with new technologies, like Artificial Intelligence and Blockchain.</p>
         </div>
     )
 }
--- src/data/Timeline.js
+++ src/data/Timeline.js
@@ -1,37 +1,24 @@
 export default [
     {
        year: 'Today',
-       title: 'YouTube Content Creator',
-       duration: '6 years',
+       title: 'Consultant',
+       duration: '2 years',
        details: 
-          'My humble attempt at building my version of the digital world one step at a time by documenting my coding journey and creating software engineering and computer science content to help those coming behind me.',
+          'Providing Cyber Security expertise to leading financial service firms including Fortune 500 companies with business solutions to develop against ongoing cyber threats.',
     },
     {
-         year: 'Today',
-         title: 'Solo Indie Developer',
-         duration: '4 years',
+         year: '2020',
+         title: 'Data Analyst Intern',
+         duration: '2 months',
          details:
-             "I'm a solo indie developer building software for myself and clients to showcase on YouTube. All coding projects are built from the ground up, from planning and designing all the way to solving real-life problems with code.",
+             "Worked in a startup and built a a deep learning model using RNN/LSTM algorithm to predict the Remaining Useful Lifetime of a Turbofan Engine",
      },
      {
-         year: '2019',
-         title: 'Software Engineer',
-         duration: '2 years',
-         details:
-             'I helped build an enterprise application for a Fortune 500 company in an Agile team setting. Our tech stack consisted of TypeSciprt, Angular, Java, Gradle, and a handful of other languages and tools.',
-     },
-     {
-         year: '2018',
+         year: '2021',
          title: "Bachelor's Degree in Computer Science",
-         duration: '5 years',
+         duration: '4 years',
          details:
              'I pursued the knowledge that a computer science degree had to offer. I learned about computer architecture, computer science theory, an array of programming, and aspects of artificial intelligence.',
      },
-     {
-         year: '2013',
-         title: 'First Line of Code',
-         duration: 'the beginning',
-         details:
-             "I wrote my first line of real code that started this journey into digital craftsmanship that I never could've imagined, especially as someone who never saw themselves as anything other than a blue collar worker.",
-     },
+
  ]
